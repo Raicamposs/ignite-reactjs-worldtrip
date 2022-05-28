60bec2410597b271093458fbf0d3df259560a60f
Raiane Campos <raiane.campos@liderautomacao.com>
2022-05-27 23:52:21 -0300

    build: config chakra-ui

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta lang="pt-BR" />
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,41 @@
+// 1. Import the extendTheme function
+import { extendTheme } from '@chakra-ui/react'
+
+
+const colors = {
+  brand: {
+    500: '#FFBA08',
+    300: 'rgba(255, 186, 8, 0.5)'
+  },
+  dark: {
+    black: '#000000',
+    font: '#47585B',
+    info: '#999999',
+    info50: 'rgba(153, 153, 153, 0.5)',
+  },
+  light: {
+    white: '##FFFFFF',
+    font: '#F5F8FA',
+    info: '#DADADA'
+  }
+
+}
+
+const fonts = {
+  heading: 'Poppins',
+  body: 'Poppins',
+}
+
+const styles = {
+  global: {
+    focus: {
+      outline: 'none',
+    },
+    body: {
+      bg: 'light.white',
+      color: 'dark.font',
+    },
+  },
+}
+
+export const theme = extendTheme({ colors, styles, fonts })
